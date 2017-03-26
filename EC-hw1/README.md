496e0076a8c98eb157aabe7578089b60fc535396
Joseph Martinsen <joseph@martinsen.com>
2017-03-26 17:28:04 -0500

    HW07 init

--- EC-hw1/README.md
+++ EC-hw1/README.md
@@ -1,1 +1,9 @@
-# CSCE222
+# CSCE222 Extra Credit 01
+by Joseph Martinsen
+
+## How to run
+
+1. Unzip me!
+2. Navigate to the directory `martinsen-joseph-EC-hw1` in terminal and run `make run`
+
+Thanks and gig'em!
--- EC-hw1/main.cpp
+++ EC-hw1/main.cpp
@@ -1,53 +1,73 @@
 #include <iostream>
 
-static int count;
-
-int gcd(int a, int b)
-{
-	if (b == 0)
-		return a;
-	return gcd(b, a % b);
-}
+int gcd(int a, int b, int& c);
 
 // Ackermann
-int A(int m, int n)
+int A(int m, int n, int& c, bool print);
+
+
+int main() {
+	int count;
+	std::cout << "GCD(12,15): " << gcd(12,15,count) << '\n';
+	std::cout << "Count: " << count << "\n\n";
+	count = 0;
+	std::cout << "GCD(228,133): " <<  gcd(228,133,count) << '\n';
+	std::cout << "Count: " << count << "\n\n";
+	count = 0;
+	std::cout << "GCD(576,414): " <<  gcd(576,414,count) << '\n';
+	std::cout << "Count: " << count << "\n\n";
+	count = 0;
+	std::cout << "GCD(1071,924): " << gcd(1071,924,count) << '\n';
+	std::cout << "Count: " << count << "\n\n";
+	count = 0;
+
+	A(1,1,count,true);
+	std::cout<<A(1,1,count,false) << '\n';
+	std::cout << "Count: " << count << "\n\n";
+	count = 0;
+
+	A(2,2,count,true);
+	std::cout<<A(2,2,count,false) << '\n';
+	std::cout << "Count: " << count << "\n\n";
+	count = 0;
+	A(2,3,count,true);
+	std::cout<<A(1,1,count,false) << '\n';
+	std::cout << "Count: " << count << "\n\n";
+	count = 0;
+
+	A(3,3,count,true);
+	std::cout<<A(1,1,count,false) << '\n';
+	std::cout << "Count: " << count << "\n\n";
+	return 0;
+}
+
+int gcd(int a, int b, int& c)
 {
-	count++;
-	std::cout << "A(" << m << "," << n << ")";
-	std::cout << " => ";
+	c++;
+	if (b == 0)
+		return a;
+	return gcd(b, a % b, c);
+}
+
+int A(int m, int n, int& c, bool print)
+{
+	c++;
+	if(print)
+	{
+		std::cout << "A(" << m << "," << n << ")";
+		std::cout << " => ";
+	}
 	if (m == 0)
 	{
 		return n + 1;
 	}
 	else if (m > 0 && n == 0)
 	{
-		return A(m-1,1);
+		return A(m-1,1,c,print);
 	}
 	else
 	{
-		int c = A(m,n-1);
-		return A(m-1,c);
+		int d = A(m,n-1,c,print);
+		return A(m-1,d,c,print);
 	}
-}
-
-
-int main() {
-	gcd(12,15);
-	std::cout << gcd(12,15) << "\n\n\n";
-	gcd(228,133);
-	std::cout << gcd(12,15) << "\n\n\n";
-	gcd(576,414);
-	std::cout << gcd(576,414) << "\n\n\n";
-	gcd(1071,924);
-	std::cout << gcd(1071,924) << "\n\n\n";
-
-	A(1,1);
-	std::cout << "\n\n\n";
-	A(2,2);
-	std::cout << "\n\n\n";
-	A(2,3);
-	std::cout << "\n\n\n";
-	A(3,3);
-	std::cout << "\n\n\n";
-	return 0;
 }
